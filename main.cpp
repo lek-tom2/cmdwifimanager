c58fc0d75a44f6262b940b37cf7843b77f9c0e09
Tomek Leks <tomek@el-jot.eu>
2024-08-06 13:13:32 +0200

    added main cpp with temp app plan

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+
+booting up app
+checks:
+if wifi is tured on
+what wlan interface are you raunning
+
+commands:
+
+list - lists aviable wifis
+list -d lists aviable wifis with thier details
+connect(name || ip || etc)
+disconnect(name || ip || etc)
+
+app will run continously printing nev wifis scanning every minute
+*/
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+    return 0;
+}
